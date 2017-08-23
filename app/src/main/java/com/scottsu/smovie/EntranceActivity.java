12d83e561351a1f319f86cea2f281aab17379cd1
su163425 <123456>
2017-08-23 16:03:16 +0800

    init project

---
+++ app/src/main/java/com/scottsu/smovie/EntranceActivity.java
@@ -0,0 +1,15 @@
+package com.scottsu.smovie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EntranceActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entrance);
+    }
+}
